78546de2d814a77e0bb4b992016ac82a7c45eb1e
Piotr Kozicki <piotr.kozicki.2022@bristol.ac.uk>
2023-07-17 14:43:28 +0100

    feat: general rejection sampling ABC

--- examples/ABC.hs
+++ examples/ABC.hs
@@ -12,77 +12,28 @@
 
 module ABC where
 
-import Probability ( RandVar(..), raise, random, uniform , uniform' )
-import Control.Monad ( replicateM )
+import Probability ( RandVar(..) )
+import Control.Monad ( replicateM, liftM2 )
 
--- * definition ripped from @Probability.hs@
---
--- * see that there is no density function for the normal distribution (!that we can access!)
-normal :: Double -> Double -> RandVar Double
-normal μ σ² =
-  let u = uniform
-      v = uniform
-      z = sqrt (-2 * log u) * cos (2 * pi * v)
-  in raise $ \gen -> do
-    z' <- random z gen
-    return (sqrt σ² * z' + μ)
-
--- restrictive summary class... in general this bit should be left to the user
--- it just needs to compose with the distance function, which I have hardcoded
-data Summary =
-  S { μ :: Double
-    , σ² :: Double
-    }
-  deriving Show
-
--- by COINCIDENCE parameters are the same as the summary
-type Parameters = Summary
-
--- | approximate bayesian computation rejection sampling
--- | extremely simple example with constant ϵ, uniform prior, decided distance function
--- | but really these should be provided by the user
--- | potentially with a step function, to vary them with every iteration!
---
--- * this example also requires μ ∈ (0, 5), σ² ∈ (0, 10)
-abc_rs :: Int -> [Double] -> Double -> RandVar [Summary]
-abc_rs 0 _ _ = return []
-abc_rs n y ϵ = raise $ \gen -> do
-  μθ <- random (uniform' 0 5) gen
-  σ²θ <- random (uniform' 0 10) gen
-  y' <- replicateM (length y) $ random (normal μθ σ²θ) gen
-  next <- random (abc_rs (n-1) y ϵ) gen
-  if dist y' <= ϵ
-    then return $ S μθ σ²θ : next
-    else return next
-  where
-    s :: [Double] -> Summary
-    s xs =
-      let mean = sum xs / fromIntegral (length xs)
-          variance = sum (map (\x -> (x - mean)**2) xs) / (fromIntegral (length xs) - 1)
-      in S mean variance
-    dist :: [Double] -> Double
-    dist y' =
-      let sy = s y
-          sy' = s y'
-      in (μ sy - μ sy')**2 + (σ² sy - σ² sy')**2
-
--- >>> import qualified System.Random.MWC as MWC
--- >>> gen <- MWC.createSystemRandom
--- >>> y <- replicateM 1000 $ random (normal 2 6) gen
--- >>> θ <- random (abc_rs 500 y 1) gen
--- >>> sum (map μ θ) / (fromIntegral $ length θ)
--- >>> sum (map σ² θ) / (fromIntegral $ length θ)
-
-{-
-EXTENSION
-- [ ] generalise summary statistic, provided by user
-- [ ] implement MCMC variant
-  - [ ] or ask user for sampling and acceptance function?
-  - all this sort of continuation can be done LATER
-  - FOR NOW I just want to generalise rejection-based sampling, implement MCMC-based sampling then do some reading on summary statistics
-
-the user WILL provide functions to work with summary statistics...
-but I will provide some example ones
-
-then I would like to make an example with a more complicated distribution
--}
+-- | Approximate Bayesian Computation via Rejection Sampling
+-- | Generates a list of "acceptable" parameters for the given generative model
+-- | Restrictions: ϵ is constant between iterations; complete resampling straight from π
+abcRejection ::
+  Int -- ^ @n@ number of iterations
+  -> [y] -- ^ @y@ real-world sample
+  -> Double -- ^ @ϵ@ maximum distance before rejection
+  -> ([y] -> s) -- ^ @s@ summary statistic function
+  -> (s -> s -> Double) -- ^ @d@ distance function
+  -> (p -> RandVar y) -- ^ @x@ generative model assumed
+  -> RandVar p -- ^ @π@ prior
+  -> RandVar [p]
+abcRejection 0 _ _ _ _ _ _ = return []
+abcRejection n y ϵ s dist m π = do
+  θ <- π
+  y' <- replicateM (length y) $ m θ
+  liftM2
+    (++)
+    (if s y `dist` s y' <= ϵ
+     then return [θ]
+     else return [])
+    (abcRejection (n-1) y ϵ s dist m π)
