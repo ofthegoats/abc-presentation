777c34f8521f812f93d9ad042ec6f249951cc453
Piotr Kozicki <piotr.kozicki.2022@bristol.ac.uk>
2023-07-24 15:02:22 +0100

    feat: implemented normal/gaussian distribution

--- ppl/PPL.hs
+++ ppl/PPL.hs
@@ -36,3 +36,9 @@ binomial' n p = length . filter id <$> replicateM n (bernoulli' p)
 type Dirac ω = Dist ω
 dirac' :: ω -> Dirac ω
 dirac' x = return x
+
+type Normal = Dist Double
+normal' :: Double -> Double -> Normal
+normal' μ σ² =
+  (\u1 u2 -> μ + σ² * sqrt (-2 * log u1) * cos (2 * pi * u2))
+  <$> uniform01 <*> uniform01
